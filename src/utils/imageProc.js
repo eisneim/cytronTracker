15cc9257f3afadda11a30dc6cfe05565d814e930
eisneim <eisneim1@gmail.com>
2016-10-25 22:35:08 +0800

    [done] add simple image process function and Sobel filter

--- src/utils/imageProc.js
+++ src/utils/imageProc.js
@@ -8,10 +8,9 @@ export function rgba2gray(imageObj) {
   const gray = [] // [ row[] row[] ]
 
   let index = 0
-  const w = imageObj.width, h = imageObj.height
-  for (let ii = 0; ii < h; ii++) {
+  for (let ii = 0; ii < imageObj.height; ii++) {
     let row = gray[ii] = []
-    for (let jj = 0; jj < w; jj++) {
+    for (let jj = 0; jj < imageObj.width; jj++) {
       row[jj] = 0.3 * data[index] + 0.59 * data[index + 1] + 0.11 * data[index + 2]
       index += 4
     }
@@ -19,6 +18,110 @@ export function rgba2gray(imageObj) {
   return gray
 }
 
-export function gray2rgba() {
-
+export function gray2rgba(grayImgArray) {
+  let data = []
+  let index = 0
+  for (var ii = 0; ii < grayImgArray.length; ii++) {
+    let row = grayImgArray[ii]
+    for (var jj = 0; jj < row.length; jj++) {
+      data[index] = row[jj]
+      data[index + 1] = row[jj]
+      data[index + 2] = row[jj]
+      data[index + 3] = 255
+      index += 4
+    }
+  }
+  return data
 }
+
+export function createMat(w, h, value) {
+  if (!w || !h) throw new Error(`invalid width or height: ${w} ${h}`)
+  let out = []
+  for (var ii = 0; ii < h; ii++) {
+    if (!out[ii]) out[ii] = []
+    for (var jj = 0; jj < w; jj++) {
+      out[ii][jj] = value
+    }
+  }
+  return out
+}
+
+export function zeros(w, h) {
+  return createMat(w, h, 0)
+}
+
+export const thresholdType = {
+  TO_ZERO: 0,
+  BINARY: 1,
+}
+
+export function threshold(gray, value, maxValue, type) {
+  let h = gray.length, w = gray[0].length
+  let out = zeros(w, h)
+  for (var ii = 0; ii < h; ii++) {
+    for (var jj = 0; jj < w; jj++) {
+      if (type === thresholdType.TO_ZERO) {
+        // thresh smaller value to zero, keep higher values
+        if (gray[ii][jj] > value)
+          out[ii][jj] = gray[ii][jj]
+      } else if (type === thresholdType.BINARY) {
+        // only keep maxValue, others will be zero, uniform to just one value
+        if (gray[ii][jj] > value)
+          out[ii][jj] = maxValue
+      }
+    }
+  }
+  return out
+}
+/**
+ * https://en.wikipedia.org/wiki/Sobel_operator
+ *  edge detection algorithms where it creates an image emphasising edges
+ *       | -1  0  1 |     1
+ * Gx =  | -2  0  2 |  =  2  * [-1 0 1]    this is for derivative of x direction
+ *       | -1  0  1 |     1
+ *
+ *       |  1  2   1|     1
+ * Gy =  |  0  0   0|  =  0  * [1 2 1]    this is for derivative of x direction
+ *       | -1 -2  -1|    -1
+ *
+ * Gradient magnitude G = sqr(Gx^2 + Gy^2)
+ * gradient direction: theta = atan(Gy / Gx)  (vertical edge theta = 0)
+ * @param {Array} imgArr .
+ * @param {int} dx .
+ * @param {int} dy .
+ * @return {ImageArray} .
+ */
+export function Sobel(imgArr, direction = 'X') {
+  const h = imgArr.length, w = imgArr[0].length
+  let out = zeros(w, h)
+  // first calculate Gx
+  if (direction === 'x') {
+    // first loop we going to mutilply 1x3 mat,  [1 / 2 / 1]
+    for (var ii = 1; ii < h - 1; ii++) { // be careful with "boundry issue"
+      for (var jj = 0; jj < w; jj++) {
+        out[ii][jj] = imgArr[ii - 1][jj] + imgArr[ii][jj] * 2 + imgArr[ii + 1][jj]
+      }
+    }
+    // second loop: get convolution with [-1 0 1]
+    for (let aa = 0; aa < h; aa++) {
+      for (let bb = 1; bb < w - 1; bb++) {
+        out[aa][bb] = -1 * imgArr[aa][bb - 1] + imgArr[aa][bb + 1]
+      }
+    }
+  } else {
+    // for y direction, first convolute with: [1 2 1]
+    for (let aa = 0; aa < h; aa++) {
+      for (let bb = 1; bb < w - 1; bb++) {
+        out[aa][bb] = imgArr[aa][bb - 1] + 2 * imgArr[aa][bb] + imgArr[aa][bb + 1]
+      }
+    }
+    // then [1 / 0 / -1]
+    for (let ii = 1; ii < h - 1; ii++) { // be careful with "boundry issue"
+      for (let jj = 0; jj < w; jj++) {
+        out[ii][jj] = imgArr[ii - 1][jj] - imgArr[ii + 1][jj]
+      }
+    }
+  } // end if else
+  return out
+}
+
--- src/utils/imageProc.spec.js
+++ src/utils/imageProc.spec.js
@@ -2,19 +2,60 @@ import { expect } from 'chai'
 import * as imageProc from './imageProc'
 
 describe('Image Process function', () => {
+  let imageObj = {
+    width: 2,
+    height: 2,
+    data: [ 0, 200, 100, 255, 0, 200, 100, 255,
+            100, 20, 50, 255, 100, 20, 50, 255 ],
+  }
   describe('rgba2gray', () => {
-    let imageData = {
-      width: 2,
-      height: 2,
-      data: [ 0, 200, 100, 255, 0, 200, 100, 255,
-              100, 20, 50, 255, 100, 20, 50, 255 ],
-    }
+
     it('converts rgba value to grayscale image array', () => {
-      let result = imageProc.rgba2gray(imageData)
+      let result = imageProc.rgba2gray(imageObj)
       expect(result).to.have.length(2)
       expect(result[0]).to.have.length(2)
     })
   })
+  describe('gray2rgba', () => {
+    it('converts grayScale value back to rgba value, but each', () => {
+      let gray = imageProc.rgba2gray(imageObj)
+      let rgba = imageProc.gray2rgba(gray)
+      expect(rgba).to.have.length(gray.length * gray[0].length * 4)
+    })
+  })
 
+  it('createMat() should create uniformed value matrix', () => {
+    let zeros = imageProc.createMat(2, 3, 0)
+    expect(zeros).to.have.length(3)
+    expect(zeros[0].length).to.equal(2)
+    expect(zeros[0][0]).to.equal(0)
+  })
+  it('threshold() should can remove high value', () => {
+    const { threshold, thresholdType } = imageProc
+    let gray = [[ 129, 129 ], [ 47.3, 47.3 ]]
+    // console.log('gray: ', gray)
+    var threshed = threshold(gray, 100, 200, thresholdType.TO_ZERO)
+    // console.log('threshed', threshed)
+    expect(threshed[0][0]).to.equal(129)
+    expect(threshed[1][0]).to.equal(0)
+  })
+  describe('Sobel() filter', () => {
+    let imgOnes = [
+      [ 100, 1, 1, 1, 100 ],
+      [ 100, 1, 1, 1, 100 ],
+      [ 100, 1, 1, 1, 100 ],
+      [ 100, 1, 1, 1, 100 ],
+      [ 100, 1, 1, 1, 100 ],
+    ]
+    it('find image edge', () => {
+      let dx = imageProc.Sobel(imgOnes, 'x')
+      //console.log(dx)
+      expect(dx[1][0] - dx[1][1]).above(100 - 1)
+      let dy = imageProc.Sobel(imgOnes, 'y')
+      // console.log(dy) // should not have much on y direction
+      expect(dy[1][0] - dy[1][1]).to.equal(0)
+      expect(dy[1][0] - dy[2][0]).to.equal(0)
+    })
+  })
 })
 
